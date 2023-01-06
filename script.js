a5bc1d7567f80fc761df61ece0c62bc0575efc76
Dismas Banda <bandadismas@gmail.com>
2023-01-06 18:09:58 +0200

    toggle menus
    Signed-off-by: Dismas Banda <bandadismas@gmail.com>

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 let smallMenu = document.getElementById("smallMenu");
+let bigMenu = document.querySelector("#bigMenu");
 let cover = document.querySelector(".cover");
+let menu = document.querySelector(".menu");
+let largeMenu = document.querySelector(".big-menu");
+
 let isMenuOpen = false;
 
 smallMenu.addEventListener("click", ()=> {
     if (!isMenuOpen) {
         cover.classList.add("show-cover");
+        menu.style.width = "300px";
         isMenuOpen = !isMenuOpen;
     } else {
         cover.classList.remove("show-cover");
+        menu.style.width = "0px";
+        isMenuOpen = !isMenuOpen;
+    }
+}, false);
+
+bigMenu.addEventListener("click", ()=> {
+    if (!isMenuOpen) {
+        cover.classList.add("show-cover");
+        largeMenu.style.width = "300px";
+        isMenuOpen = !isMenuOpen;
+    } else {
+        cover.classList.remove("show-cover");
+        largeMenu.style.width = "0px";
         isMenuOpen = !isMenuOpen;
     }
 }, false);
 
 cover.addEventListener("click", () => {
     cover.classList.remove("show-cover");
+    menu.style.width = "0px";
+    largeMenu.style.width = "0px";
     isMenuOpen = false;
 }, false);
